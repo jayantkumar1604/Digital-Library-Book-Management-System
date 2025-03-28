6982154f4e0a29e967359841f4282871f1ebf6ca
Jayant Kumar <123481470+jayantkumar1604@users.noreply.github.com>
2025-03-28 06:49:44 +0530

    Delete DigitalLibraryBookManagementSystem directory

--- DigitalLibraryBookManagementSystem/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO BOOK (BOOK_ID, TITLE, AUTHOR, GENRE, STATUS)
-VALUES ('B001', 'Spring Boot Basics', 'John Doe', 'Tech', 'Available');
-
-INSERT INTO BOOK (BOOK_ID, TITLE, AUTHOR, GENRE, STATUS)
-VALUES ('B002', 'Java Fundamentals', 'Jane Smith', 'Programming', 'Checked Out');
